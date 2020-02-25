7106eb42b1737a4ff24163232d8a066b63659906
mashable <michaeltournaud@gmail.com>
2020-02-25 09:49:54 +0100

    added docker files

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,15 @@
-Dockerfile
+FROM node:alpine as builder
+
+WORKDIR '/app'
+
+COPY package.json .
+
+RUN npm install
+
+COPY . .
+
+RUN npm run build
+
+FROM nginx:alpine
+
+COPY --from=builder /app/build /usr/share/nginx/html
--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there bitch bibou DF NETTE !
+          Hi there Bibounette !
         </p>
         <a
           className="App-link"
